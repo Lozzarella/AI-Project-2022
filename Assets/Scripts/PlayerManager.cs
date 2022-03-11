cf88fe5d7e821ab2cbfd95befd5d08a8cec0bc16
Lozzarella <laurenbrooks70@gmail.com>
2022-03-11 13:54:23 +1100

    New StateMachine

---
+++ Assets/Scripts/PlayerManager.cs
@@ -0,0 +1,76 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager : BaseManager
+{
+    [SerializeField] protected CanvasGroup _buttonGroup;
+    protected AIManager _aiManager;
+    protected bool _isHealOverTimeRunning = false;
+    protected int healTimesRemaining = 0;
+
+    protected override void Start()
+    {
+        base.Start();
+        _aiManager = GetComponent<AIManager>();
+        if (_aiManager == null)
+        {
+            Debug.LogError("AIManager no found");
+        }
+    }
+
+    public override void TakeTurn()
+    {
+        _buttonGroup.interactable = true;
+    }
+
+    public void EndTurn()
+    {
+        _buttonGroup.interactable = false;
+        _aiManager.TakeTurn();  
+    }
+
+    public void EatBerries()
+    {
+        Heal(20f);
+        StartCoroutine(HealOverTime(3,1f));
+    }
+
+    
+    private IEnumerator HealOverTime(int times, float waitTime)
+    {
+        healTimesRemaining += times;
+
+        if (_isHealOverTimeRunning == false)
+        {
+            _isHealOverTimeRunning = true;
+
+
+            while (healTimesRemaining > 0)
+            {
+                Heal(10f);
+                yield return new WaitForSeconds(waitTime);
+                healTimesRemaining--;
+            }
+            _isHealOverTimeRunning = false;
+        }
+    }
+    public void SelfDestruct()
+    {
+        DealDamage(_maxHealth);
+        _aiManager.DealDamage(80f);
+        EndTurn();
+    }
+
+    public void VineWhip()
+    {
+        _aiManager.DealDamage(30f);
+        EndTurn();
+    }
+
+    public void FlameWheel()
+    {
+        _aiManager.DealDamage(50f);
+        EndTurn();
+    }
+}
